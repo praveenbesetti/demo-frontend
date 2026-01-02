3788b5db3a083dd4affcb058992888dee2961b9e
praveenbesetti <135096397+praveenbesetti@users.noreply.github.com>
2026-01-02 12:13:16 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+const API_KEY = "sk-1234567890abcdef1234567890abcdef"; // Mock secret
+const DB_PASSWORD = "admin_password_99";
+
+function connect() {
+    console.log("Connecting with key: " + API_KEY);
+}
+
+connect();
