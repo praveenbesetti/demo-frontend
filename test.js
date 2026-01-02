b762cd91d826b92b654c88694f6b4e98320f908d
praveenbesetti <135096397+praveenbesetti@users.noreply.github.com>
2026-01-02 13:50:20 +0530

    Add additional console log for debugging
    Added a console log for debugging purposes.

--- test.js
+++ test.js
@@ -26,6 +26,7 @@ function authenticateUser(inputUser, inputPass) {
     }
 }
 console.log("wrfkmwkfwv");
+console.log("wrfkmwkfwvrgreg");
 
 // 6. LOGIC BUG: The function is called but result is never used
 authenticateUser("admin", "12345");
