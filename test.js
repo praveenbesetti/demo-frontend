d99682cc92b149c05a672b42210471c73cabf666
praveenbesetti <135096397+praveenbesetti@users.noreply.github.com>
2026-01-05 10:05:00 +0530

    Log request object in status endpoint

--- test.js
+++ test.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.get('/status', (req, res) => {
     // Standard status check
+
+    console.log(req);
     res.status(200).json({ status: "online", timestamp: new Date() });
 });
 
